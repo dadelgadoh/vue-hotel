d1eb4ce5666f166f27236e8ab28bd7c625c7e509
Diego Delgado <dadelgadoh@unal.edu.co>
2023-08-20 23:11:48 -0500

    Selects en Editar habitacion

--- src/views/rooms/RoomEdit.vue
+++ src/views/rooms/RoomEdit.vue
@@ -1,9 +1,11 @@
 
 <script setup>
+import useHotels from '../../composables/hotels';
 import useRooms from '../../composables/rooms';
 import { onMounted } from 'vue';
 
 const { room, getRoom, updateRoom, errors } = useRooms();
+const { hotels, getHotels } = useHotels();
 
 const props = defineProps(
     {
@@ -13,8 +15,13 @@ const props = defineProps(
         }
     }
 )
-onMounted(() => getRoom(props.id))
+// onMounted(() => getRoom(props.id))
+onMounted(() => {
+      getHotels();
+      getRoom(props.id);
+    });
 </script>
+
 
 <template>
     <div class="mt-12">
@@ -22,33 +29,53 @@ onMounted(() => getRoom(props.id))
             <div class="space-y-6">
                 <div class="mb-6">
                     <label for="hotel" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Hotel</label>
-                    <input type="text" id="hotel" v-model="room.hotel_id"
-                        class="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                    <div v-if="errors.hotel_id"><span class="text-sm text-red-400">{{ errors.hotel_id[0] }}</span></div>
+                    <select id="hotel" v-model="room.hotel_id" 
+                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                        <option disabled selected>Escoja el hotel</option>
+                        <option v-for="hotelSelect in hotels" :key="hotelSelect.id" :value="hotelSelect.id">{{ hotelSelect.name }}</option>
+                    </select>
                 </div>
                 <div class="mb-6">
-                    <label for="room_type"
-                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Tipo de habitación</label>
-                    <input type="text" id="room_type" v-model="room.room_type_id"
-                        class="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                    <div v-if="errors.room_type_id"><span class="text-sm text-red-400">{{ errors.room_type_id[0] }}</span></div>
+
+                    <label for="room_type" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Tipo de
+                        habitación</label>
+                    <select id="room_type" v-model="room.room_type_id"
+                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                        <option disabled selected>Escoja el tipo</option>
+                        <option value="1">Estándar</option>
+                        <option value="2">Junior</option>
+                        <option value="3">Suite</option>
+                    </select>
+
+                    <div v-if="errors.room_type_id"><span class="text-sm text-red-400">{{ errors.room_type_id[0] }}</span>
+                    </div>
                 </div>
                 <div class="mb-6">
-                    <label for="accommodation" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Acomodación</label>
-                    <input type="text" id="accommodation" v-model="room.accommodation_id"
-                        class="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                    <div v-if="errors.accommodation_id"><span class="text-sm text-red-400">{{ errors.accommodation_id[0] }}</span></div>
+                    <label for="accommodation"
+                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Acomodación</label>
+                    <select id="accommodation" v-model="room.accommodation_id"
+                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                        <option disabled selected>Escoja la acomodación</option>
+                        <option value="1">Sencilla</option>
+                        <option value="2">Doble</option>
+                        <option value="3">Triple</option>
+                        <option value="4">Cuádruple</option>
+                    </select>
+
+                    <div v-if="errors.accommodation_id"><span class="text-sm text-red-400">{{ errors.accommodation_id[0]
+                    }}</span></div>
                 </div>
                 <div class="mb-6">
-                    <label for="quantity" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Cantidad</label>
-                    <input type="text" id="quantity" v-model="room.quantity"
+                    <label for="quantity"
+                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Cantidad</label>
+                    <input type="number" id="quantity" v-model="room.quantity"
                         class="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                     <div v-if="errors.quantity"><span class="text-sm text-red-400">{{ errors.quantity[0] }}</span></div>
                 </div>
 
                 <div class="mt-4">
                     <button type="submit"
-                        class="px-4 py-2 bg-indigo-500 hover:bg-indigo-700 text-white rounded">Crear</button>
+                        class="px-4 py-2 bg-indigo-500 hover:bg-indigo-700 text-white rounded">Guardar</button>
                 </div>
             </div>
         </form>
